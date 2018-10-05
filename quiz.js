f81e29f2361def2604a59b89c4b7d23a044b84ff
MJ Phillip <mj@resafrica.com>
2018-10-05 08:02:10 +0100

    Added options to page and shuffled them
    Next I need to get the addEventListener on to check if answers are correct.

--- quiz.js
+++ quiz.js
@@ -10,5 +10,27 @@ function showQuestions(result){
     var questionArray = result.results;
     document.getElementById('questions').innerHTML += questionArray[0].question;
 
-    document.getElementById('answers').innerHTML += questionArray[0].correct_answer + questionArray[0].incorrect_answers[0] + questionArray[0].incorrect_answers[1] + questionArray[0].incorrect_answers[2];
-}
+    var answerArray = [questionArray[0].correct_answer,questionArray[0].incorrect_answers[0],questionArray[0].incorrect_answers[1],questionArray[0].incorrect_answers[2]]
+
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    shuffle(answerArray);
+
+    for(var i=0; i < answerArray.length; i++){
+      document.getElementById('answers').innerHTML += "<input type='submit' onclick='checkAnswer()' value='" + answerArray[i] + "'>";
+    };
+
+    function checkAnswer(){
+      console.log("Hi")
+    }
+
+};
